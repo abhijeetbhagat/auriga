4ba0f0d1c5207e236c3757de8bd88a741749284f
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-02-16 19:07:45 +0530

    Split test client into consumer and producer

--- src/clients/consumer.rs
+++ src/clients/consumer.rs
@@ -7,11 +7,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let server_addr = "127.0.0.1:61616";
     println!("Connecting to {} ...", server_addr);
     let mut stream = TcpStream::connect(server_addr).await?; 
-    println!("Sending SUBSCRIBE to server ...");
+    println!("Consumer: Sending SUBSCRIBE to server ...");
     stream.write_all(b"SUBSCRIBE\nid:0\ndestination:/queue/foo\nack:client\n").await?;
-    //stream.write_all(b"SUBSCRIBE\n").await?;
     let mut buf = [0; 50];
-    stream.read(&mut buf).await?;
-    println!("{}", std::str::from_utf8(&buf).unwrap());
+    loop {
+        println!("Consumer: Waiting for message...");
+        stream.read(&mut buf).await?;
+        println!("Consumer: Message recvd - {}", std::str::from_utf8(&buf).unwrap());
+    }
     Ok(())
 }
